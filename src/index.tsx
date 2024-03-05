2418ad3e0b2016920c34481d61b380b177a6db1e
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-03-05 18:44:48 +0100

    añadido ejemplo json

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,10 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/json", (c) => {
+  return c.json({ message: "Hola Hono!" });
+});
+
 app.get("/salida", (c) => {
   const messages = ["Good Morning", "Good Evening", "Good Night"];
   return c.html(<Top messages={messages} />);
