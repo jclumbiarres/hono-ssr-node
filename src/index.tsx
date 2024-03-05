f5de9108c9cb592d6ad8bfa57908b3953ea8bf89
Juan Carlos Lumbiarres <jclumbiarres@protonmail.com>
2024-03-05 18:41:22 +0100

    ejemplo

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+// Ejemplo de la documentación oficial de Hono para crear un servidor web con Hono y JSX.
+
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { Top } from "./components/Hola";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/salida", (c) => {
+  const messages = ["Good Morning", "Good Evening", "Good Night"];
+  return c.html(<Top messages={messages} />);
+});
+
+const port = 3000;
+
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
